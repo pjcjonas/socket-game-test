c1a28ff51ffd897cd075d466296c7344cf51cbf4
pjcjonas <pjcjonas@gmail.com>
2020-11-01 10:54:43 +0200

    INtegrated basic phaser scene

--- src/game/modules/world/index.ts
+++ src/game/modules/world/index.ts
@@ -4,25 +4,12 @@ export class GameWorld extends Phaser.Scene {
         super('GameWorld');
     }
 
-    preload () {
-        this.load.image('logo', 'https://raw.githubusercontent.com/photonstorm/phaser3-typescript-project-template/master/dist/assets/phaser3-logo.png');
-        this.load.image('libs', 'https://raw.githubusercontent.com/photonstorm/phaser3-typescript-project-template/master/dist/assets/libs.png');
+    preload = (): void => {
     }
 
-    create () {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-        this.add.image(400, 300, 'libs');
+    create = (): void => {
+    }
 
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+    update = (): void => {
     }
 }
